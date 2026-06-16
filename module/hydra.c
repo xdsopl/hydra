03f8273ba43764c83a7bf77b884aa34d59037e73
Ahmet Inan <xdsopl@gmail.com>
2026-06-16 08:51:42 +0200

    compute node hashes

--- module/hydra.c
+++ module/hydra.c
@@ -5,7 +5,6 @@ Copyright 2026 Ahmet Inan <xdsopl@gmail.com>
 */
 
 #include "blake2.h"
-#include "string.h"
 
 #define MLEN_MAX (1 << 20)
 #define HLEN_MAX (16 << 9)
@@ -38,16 +37,25 @@ int digest_blocks(int height, int blen)
 {
 	if (height < 2 || height > 8)
 		return 1;
-	int bcnt = 1 << height;
-	if (blen < 0 || blen * bcnt > MLEN_MAX)
+	int count = 1 << height;
+	if (blen < 0 || blen * count > MLEN_MAX)
 		return 1;
 	blake2s_state init, state;
 	unsigned char key = 0;
 	blake2s_init_key(&init, 16, &key, 1);
-	for (int i = 0; i < bcnt; i++) {
-		memcpy(&state, &init, sizeof(blake2s_state));
+	for (int i = 0; i < count; i++) {
+		state = init;
 		blake2s_update(&state, blocks+blen*i, blen);
 		blake2s_final(&state, hashes+16*i, 16);
+	}
+	key = 1;
+	blake2s_init_key(&init, 16, &key, 1);
+	for (int prev = 0, offset = count; count > 1; prev = offset, offset += count /= 2) {
+		for (int i = 0; i < count / 2; i++) {
+			state = init;
+			blake2s_update(&state, hashes+prev+32*i, 32);
+			blake2s_final(&state, hashes+offset+16*i, 16);
+		}
 	}
 	return 0;
 }
